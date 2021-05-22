6da05511b82ca0fe33cad1209fe6b2f4f2b14290
Leomiguel19 <leomiguel1907@gmail.com>
2021-05-21 21:58:50 -0400

    Antipatron SOLID

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,33 @@ import './App.css'
 const App = () => {
   // Lo que ejecuta la función
   return (
-  <div className="react-calculator">
-    <h1> Calc App </h1>
-  </div>)
+    <main className="react-calculator">
+      <div className="result">
+      </div>
+      <div className="numbers">
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>0</button>
+      </div>
+      <div className="functions">
+        <button>clear</button>
+        <button>r</button>
+      </div>
+      <div className="math-operations">
+        <button>+</button>
+        <button>-</button>
+        <button>*</button>
+        <button>/</button>
+        <button>=</button>
+      </div>
+    </main>)
 }
 // Exportación
 export default App;
