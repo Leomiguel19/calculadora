5f9e8b9f7f07176e11ab9e01e757f08c3315c75b
Leomiguel19 <leomiguel1907@gmail.com>
2021-05-23 18:08:37 -0400

    Creado el componente Numbers

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,28 @@
 import React from 'react'
-import Button from './components/Button'
 import Functions from './components/Functions'
 import MathOperations from './components/MathOperations'
+import Numbers from './components/Numbers'
 import Result from './components/Result'
 import './App.css'
 
 // Función flecha o Arrow Function
 const App = () => {
-  
-  const clickHandlerFunction = text => {
-    console.log("Button.clickHandler", text)
-  }
   // Lo que ejecuta la función
   return (
     <main className="react-calculator">
       <Result value={undefined}/>
-      <div className="numbers">
-        <Button text='1' clickHandler = {clickHandlerFunction} />
-        <button>2</button>
-        <button>3</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button>0</button>
-      </div>
+      <Numbers
+        onClickNumber={number =>
+        console.log("Click en el número: ", number)
+        }
+      />
       <Functions 
         onContentClear={() =>
           console.log("Content clear")
         }
-        onDelete={() => {
+        onDelete={() => 
           console.log("OnDelete")
-        }}
+        }
       />
       <MathOperations 
         onClickOperation={operation => 
