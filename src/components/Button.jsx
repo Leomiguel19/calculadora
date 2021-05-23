345f5a8f27e6119147247340a2cede887065934f
Leomiguel19 <leomiguel1907@gmail.com>
2021-05-22 22:30:30 -0400

    PropTypes en Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const Button = ({type, text, clickHandler}) => {
   return (
     <button className={type} onClick={() => {
-      console.log('onClick en el Button')
       clickHandler(text)
     }}>
       <span>{text}</span>
     </button>
   )
+}
+
+Button.propTypes = {
+  type: PropTypes.string,
+  text: PropTypes.string.isRequired,
+  clickHandler: PropTypes.func.isRequired
 }
 
 export default Button 
