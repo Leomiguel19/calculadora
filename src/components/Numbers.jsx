bbd8a4de68362f6af15e5cf263f1a8533df803a7
Leomiguel19 <leomiguel1907@gmail.com>
2021-05-23 18:23:12 -0400

    DRY aplicado: renderButtons

--- src/components/Numbers.jsx
+++ src/components/Numbers.jsx
@@ -2,18 +2,19 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Button from './Button'
 
+// DRY: Don't repeat yourself
+const renderButtons = onClickNumber => {
+  // var number = 0
+  // iterar desde el botón 1 al 10
+  // var ArrayComponentes +=
+  // <Button text={number.toString()} clickHandler={onClickNumber}
+}
+
 const Numbers = ({onClickNumber}) => (
   <section className="numbers">
-    <Button text="1" clickHandler={onClickNumber}/>
-    <Button text="2" clickHandler={onClickNumber}/>
-    <Button text="3" clickHandler={onClickNumber}/>
-    <Button text="4" clickHandler={onClickNumber}/>
-    <Button text="5" clickHandler={onClickNumber}/>
-    <Button text="6" clickHandler={onClickNumber}/>
-    <Button text="7" clickHandler={onClickNumber}/>
-    <Button text="8" clickHandler={onClickNumber}/>
-    <Button text="9" clickHandler={onClickNumber}/>
-    <Button text="0" clickHandler={onClickNumber}/>
+    {
+      renderButtons(onClickNumber)
+    }
   </section>
 )
 
